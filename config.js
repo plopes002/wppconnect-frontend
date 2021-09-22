c9606fb04ef8d629a731577a6c8a8368fde855f1
Paulo <84867289+plopes002@users.noreply.github.com>
2021-09-22 14:25:56 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.IP_SERVER = "http://wpp.igordisco.com:21466/api/";
+window.IP_SOCKET_IO = "http://wpp.igordisco.com:21466/";
+
+//window.IP_SERVER = "http://localhost:21465/api/";
+//window.IP_SOCKET_IO = "http://localhost:21465/";
+
+//window.IP_SERVER = "http://192.168.15.46:21465/api/";
+//window.IP_SOCKET_IO = "http://192.168.15.46:21465/";
